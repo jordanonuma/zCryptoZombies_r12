7fc6f3baf284149033bd180a2246c035870cf67f
jordan <jordanonuma@outlook.com>
2022-09-19 08:02:11 -1000

    Revised boolean.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -4,7 +4,7 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-        require(msg.sender = zombieToOwner[_zombieId]);
+        require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
     } //end function feedAndMultiply()
 } //end contract ZombieFeeding{}
