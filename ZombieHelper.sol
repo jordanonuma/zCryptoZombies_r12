934d42209b1d2b529e3fa29eb14b4e0d632a55ef
jordan <jordanonuma@outlook.com>
2022-10-03 08:15:52 -1000

    Added functions that allow user to edit their zombie's name and DNA if level requirements are met.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -5,8 +5,18 @@ import "./ZombieFeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
     modifier aboveLevel(uint _level, uint _zombieId) {
-        require(zombies[_zombieId].level >_level);
+        require(zombies[_zombieId].level >= _level);
         _;
     } //end modifier aboveLevel()
+
+    function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    } //end function changeName()
+
+    function changeDna(uint _zombieId, string calldata _newDna) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    } //end function changeDna()
 
 } //end contract ZombieHelper{}
