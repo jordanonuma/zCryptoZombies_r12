e7d2d6c2ca6dc3de072bccd818c48ba2900083da
Jordan <jordanonuma@outlook.com>
2022-10-12 13:46:51 -1000

    Defined probability of victories and frame of function for zombie attacks.

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -3,6 +3,12 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
     uint randNonce = 0;
+    uint attackVictoryProbability = 70;
+
+    function attack(uint _zombieId, uint _targetId) external {
+        
+    } //end function attack()
+
     function randMod(uint _modulus) internal returns(uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
