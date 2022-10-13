897f3d17c47c65d23c14e7c0a2375f394c4f685e
jordan <jordanonuma@outlook.com>
2022-10-13 13:49:33 -1000

    Added modifier that checks if user is owner.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -21,6 +21,11 @@ contract ZombieFeeding is ZombieFactory {
 
     KittyInterface kittyContract;
 
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    } //end modifier ownerOf()
+
     function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     } //end function setKittyContractAddress()
@@ -33,8 +38,7 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     } //end function _isReady()
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
 
