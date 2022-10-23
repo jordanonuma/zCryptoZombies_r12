975fef5e251c1ef395c79356d0148c811e278089
jordan <jordanonuma@outlook.com>
2022-10-23 08:04:15 -1000

    Added function that allows zombie ownership to transfer.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -11,6 +11,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         return zombieToOwner[_tokenId];
     } //end function ownerOf()
 
+    function _transfer(address _from, address _to, uint256 _tokenid) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    } //end function _transfer()
+
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
 
     } //end function transferFrom()
