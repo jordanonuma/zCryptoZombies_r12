9f32177a1f567bc586de0a450f3ed54d12ca5130
jordan <jordanonuma@outlook.com>
2022-10-24 11:35:31 -1000

    Revised so owner can send or the approved recipient can initiate the transfer.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -21,7 +21,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     } //end function _transfer()
 
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-        require(_to == zombieApprovals[_tokenId]);
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
     _   _transfer(_from, _to, _tokenId);
     } //end function transferFrom()
 
