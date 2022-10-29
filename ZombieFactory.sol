d937a93ac26cc0f71097eaa02fd4f696a811b527
jordan <jordanonuma@outlook.com>
2022-10-29 09:25:17 -1000

    Corrected declaration.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -4,8 +4,8 @@ import "./SafeMath.sol";
 
 contract ZombieFactory is Ownable {
     using SafeMath for uint256;
-    using SafeMath for uint32;
-    using SafeMath for uint16;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
 
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
