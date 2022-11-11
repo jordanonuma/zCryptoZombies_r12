6c19ae92fca359db33865324136b41fa690f71f0
jordan <jordanonuma@outlook.com>
2022-11-11 10:54:58 -1000

    Added frame of function that will be enabled before the match's start.

--- Zombie Game Modes/ExampleGame.sol
+++ Zombie Game Modes/ExampleGame.sol
@@ -3,6 +3,8 @@ import "ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
-    
+    function beforeMatchStart(bytes SerializedGameState) external {
+        
+    } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
