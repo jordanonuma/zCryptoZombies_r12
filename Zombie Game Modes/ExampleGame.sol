07210ac7569f6c6739ea2bcb5417f0807f17dfbb
jordan <jordanonuma@outlook.com>
2022-11-14 11:56:25 -1000

    Declared custom data type so states can be passed back to the server.

--- Zombie Game Modes/ExampleGame.sol
+++ Zombie Game Modes/ExampleGame.sol
@@ -6,6 +6,9 @@ contract ExampleGame is ZBGameMode  {
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
         gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
+        changes.init();
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
