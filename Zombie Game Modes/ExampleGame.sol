40ca04a21e630f06cbd234935b2e43b5120a827f
jordan <jordanonuma@outlook.com>
2022-11-15 10:02:33 -1000

    Redefined defaults to the 'changes' data type.

--- Zombie Game Modes/ExampleGame.sol
+++ Zombie Game Modes/ExampleGame.sol
@@ -9,6 +9,18 @@ contract ExampleGame is ZBGameMode  {
 
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
+
+        changes.changePlayerDefense(Player.Player1, 15);
+        changes.changePlayerDefense(Player.Player2, 15);
+
+        changes.changeCurrentGooVials(Player.Player1, 3);
+        changes.changeCurrentGooVials(Player.Player2, 3);
+
+        changes.changeCurrentGoo(Player.Player1, 3);
+        changes.changeCurrentGoo(Player.Player2, 3);
+
+        changes.changeMaxGooVials(Player.Player1, 8);
+        changes.changeMaxGooVials(Player.Player2, 8);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
