3997723371cb34047dc653e09ffedb888e9d04cd
jordan <jordanonuma@outlook.com>
2022-11-15 10:05:26 -1000

    Corrected method names applied to the data type.

--- Zombie Game Modes/ExampleGame.sol
+++ Zombie Game Modes/ExampleGame.sol
@@ -12,15 +12,15 @@ contract ExampleGame is ZBGameMode  {
 
         changes.changePlayerDefense(Player.Player1, 15);
         changes.changePlayerDefense(Player.Player2, 15);
+        
+        changes.changePlayerCurrentGooVials(Player.Player1, 3);
+        changes.changePlayerCurrentGooVials(Player.Player2, 3);
 
-        changes.changeCurrentGooVials(Player.Player1, 3);
-        changes.changeCurrentGooVials(Player.Player2, 3);
+        changes.changePlayerCurrentGoo(Player.Player1, 3);
+        changes.changePlayerCurrentGoo(Player.Player2, 3);
 
-        changes.changeCurrentGoo(Player.Player1, 3);
-        changes.changeCurrentGoo(Player.Player2, 3);
-
-        changes.changeMaxGooVials(Player.Player1, 8);
-        changes.changeMaxGooVials(Player.Player2, 8);
+        changes.changePlayerMaxGooVials(Player.Player1, 8);
+        changes.changePlayerMaxGooVials(Player.Player2, 8);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
