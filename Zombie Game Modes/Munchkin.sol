4231cc7a8738e2a8981dd4afd55149023d94c36e
Jordan <jordanonuma@outlook.com>
2022-11-23 14:09:33 -1000

    Added checks for card playable. Defined frame of function that checks if card is playable.

--- Zombie Game Modes/Munchkin.sol
+++ Zombie Game Modes/Munchkin.sol
@@ -13,8 +13,26 @@ contract Munchkin is ZBGameMode  {
         changes.init();
 
         // Custom game logic will go here
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
 
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                }
+            }
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount); 
+        }
+
+        
         changes.emit();
 
     } //end function beforeMatchStart()
+
+    function isLegalCard(CardInstance card) internal view returns(bool) {
+        
+    } //end function isLegalCard()
+
 } //end contract Munchkin{}
