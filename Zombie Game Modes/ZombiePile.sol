f5c81972cd6ce0cf607c418baa661f28960e5bde
jordan <jordanonuma@outlook.com>
2022-11-28 13:46:02 -1000

    Assigns cards based on randomized integer.

--- Zombie Game Modes/ZombiePile.sol
+++ Zombie Game Modes/ZombiePile.sol
@@ -15,25 +15,21 @@ contract ZombiePile is ZBGameMode  {
 
         CardInstance[] memory player1Cards = new CardInstance[](gameState.playerStates[0].cardsInDeck.length);
         CardInstance[] memory player2Cards = new CardInstance[](gameState.playerStates[1].cardsInDeck.length);
-        uint player1Cards = 0;
-        uint player2Cards = 0;
+        uint player1CardCount = 0;
+        uint player2CardCount = 0;
 
         for (uint i = 0; i < gameState.playerStates.length; i++) {
             
 
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                bool cardAlreadyInDeck = false;
-
-                for (uint k = 0; k < cardCount; k++) {
-                    if (keccak256(abi.encodePacked(newCards[k].mouldName)) == keccak256(abi.encodePacked(gameState.playerStates[i].cardsInDeck[j].mouldName))) {
-                        cardAlreadyInDeck = true;
-                    } //end if()
-                } //end for(k)
-
-                if (!cardAlreadyInDeck) {
-                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
-                    cardCount++;
-                } //end if()
+                uint rand = uint(keccak256(abi.encodePacked(now, player1CardCount + player2CardCount))) % 2;
+                if (rand == 0) {
+                    player1Cards[player1CardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    player1CardCount++;
+                } else {
+                    player2Cards[player2CardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    player2CardCount++;
+                } // end if-else()
 
             } //end for(j)
 
