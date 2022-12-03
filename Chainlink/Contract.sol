e161e06589a4ed4d275203049a8862eb5713cc48
jordan <jordanonuma@outlook.com>
2022-12-03 13:13:01 -1000

    Instantiates the Chainlink interface.

--- Chainlink/Contract.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.7;
-
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-
-contract PriceConsumerV3 {
-
-} //end contract PricerConsumerV3{}
---
+++ Chainlink/PriceConsumerV3.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.7;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface public priceFeed;
+
+    constructor {
+        priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8);
+    } //end constructor{}
+
+} //end contract PriceConsumerV3{}
