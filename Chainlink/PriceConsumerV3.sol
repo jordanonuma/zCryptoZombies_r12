3de2a0ffbc9170dc8c697a7d0de354b49b275da0
jordan <jordanonuma@outlook.com>
2022-12-04 14:13:50 -1000

    Fetches data.

--- Chainlink/PriceConsumerV3.sol
+++ Chainlink/PriceConsumerV3.sol
@@ -5,8 +5,12 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 contract PriceConsumerV3 {
     AggregatorV3Interface public priceFeed;
 
-    constructor {
+    constructor() public {
         priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8);
     } //end constructor{}
 
+    function getLatestPrice() public view returns(int) {
+        (,int price,,,) = priceFeed.latestRoundData();
+        return price;
+    } //end function getLatestPrice()
 } //end contract PriceConsumerV3{}
