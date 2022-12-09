73c06f1ce25750d72bcb6285fd571d5e21291509
jordan <jordanonuma@outlook.com>
2022-12-08 17:27:57 -1000

    Defines the VRF variables.

--- Chainlink/ZombieFactory.sol
+++ Chainlink/ZombieFactory.sol
@@ -7,11 +7,16 @@ contract ZombieFactory is VRFConsumerbase {
     constructor() VRFConsumerBase(
         0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B,
         0x01BE23585060835E02B77ef475b0Cc51aA1e0709) public {
-            
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 100000000000000000;
     } //end constructor()
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    bytes32 public keyHash;
+    uint256 public fee;
+    uint256 public randomResult;
 
     struct Zombie {
         string name;
