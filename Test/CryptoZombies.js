cdd42e3cb9eee8340188921b5225c259e80b55e5
jordan <jordanonuma@outlook.com>
2022-12-15 13:43:05 -1000

    Initializes the test with account abstraction.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -4,5 +4,6 @@ const CryptoZombies = artifacts.require("CryptoZombies");
       let [alice, bob] = accounts;
 
       it("should be able to create a new zombie", async () => {
+         const contractInstance = await CryptoZombies.new();
       }) //end it()
  }) //end contract()
