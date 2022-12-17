9609dd4ceeffe33d1b9e0b0c26f74c083ecdd28d
jordan <jordanonuma@outlook.com>
2022-12-16 14:47:57 -1000

    Checks if the trasnsaction has gone through and the zombie name has been logged.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -2,8 +2,12 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 
  contract("CryptoZombies", (accounts) => {
       let [alice, bob] = accounts;
+      const zombieNames = ["Zombie 1", "Zombie 2"];
 
       it("should be able to create a new zombie", async () => {
          const contractInstance = await CryptoZombies.new();
+         const result = contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+         assert.equal(result.receipt.status, true);
+         assert.equal(result.logs[0].args.name, zombieNames[0]);
       }) //end it()
  }) //end contract()
