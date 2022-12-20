d7738f0891954e42a5964d82d2a7b94c055daaa6
jordan <jordanonuma@outlook.com>
2022-12-20 09:33:39 -1000

    Added tests for transferring zombies.

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -18,6 +18,21 @@ const zombieNames = ["Zombie 1", "Zombie 2"];
 
       it("should not allow two zombies", async () => {
          await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
+         await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
       }) //end it()
+
+      xcontext("with the single-step transfer scenario", async () => {
+         it("should transfer a zombie", async () => {
+           // TODO: Test the single-step transfer scenario.
+         })
+      })
+     
+     xcontext("with the two-step transfer scenario", async () => {
+         it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
+           // TODO: Test the two-step scenario.  The approved address calls transferFrom
+         })
+         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+             // TODO: Test the two-step scenario.  The owner calls transferFrom
+          })
+     })
  }) //end contract()
