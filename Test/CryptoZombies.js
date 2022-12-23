8648348b9498d7bb776d6b1c199b499b04a013b3
jordan <jordanonuma@outlook.com>
2022-12-22 19:07:51 -1000

    Tests case where Bob calls transferFrom().

--- Test/CryptoZombies.js
+++ Test/CryptoZombies.js
@@ -23,15 +23,24 @@ const zombieNames = ["Zombie 1", "Zombie 2"];
 
       context("with the single-step transfer scenario", async () => { //did change go through
          it("should transfer a zombie", async () => {
-           // TODO: Test the single-step transfer scenario.
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner, bob);
          }) //end it()
       }) //end context()
      
-      xcontext("with the two-step transfer scenario", async () => {
+      context("with the two-step transfer scenario", async () => {
          it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
-           // TODO: Test the two-step scenario.  The approved address calls transferFrom
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
          }) //end it()
-         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+         xit("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
              // TODO: Test the two-step scenario.  The owner calls transferFrom
          }) //end it()
      }) //end context()
