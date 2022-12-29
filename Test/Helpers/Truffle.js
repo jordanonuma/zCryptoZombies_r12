516410f9e1cf8038b5858dd4d9854f6b1ed9b781
jordan <jordanonuma@outlook.com>
2022-12-29 13:50:23 -1000

    Initialized truffle wallet hd provider for testing.

--- Test/Helpers/Truffle.js
+++ Test/Helpers/Truffle.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("truffle-hdwallet-provider"); //for testing
 const LoomTruffleProvider = require('loom-truffle-provider');
 const mnemonic = "YOUR MNEMONIC HERE";
 module.exports = {
