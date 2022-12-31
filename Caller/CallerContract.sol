4eef28f2415dd1935f163224dc9aac26571c1365
jordan <jordanonuma@outlook.com>
2022-12-31 12:00:43 -1000

    Limited instantiation to within contract.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -2,8 +2,9 @@ pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
 
 contract CallerContract {
-    EthPriceOracleInterface public oracleInstance;
+    EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
