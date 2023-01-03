6de01d07d7b8995abbe0a45480da3981c859b298
Jordan <jordanonuma@outlook.com>
2023-01-02 17:23:30 -1000

    Added function that calls the oracle instance and lets front end know.

--- Caller/CallerContract.sol
+++ Caller/CallerContract.sol
@@ -5,11 +5,19 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract CallerContract {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256=>bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
     } //end function setOracleInstanceAddress()
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequest[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
+    } //end function updateEthPrice()
 } //end contract CallerContract{}
