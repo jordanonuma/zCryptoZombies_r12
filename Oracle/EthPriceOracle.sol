18c68e0e368f132d0bbad082bd8ec959ed4c5bdf
jordan <jordanonuma@outlook.com>
2023-01-28 10:03:37 -1000

    Summates oracle responses for a given ID and takes the average.

--- Oracle/EthPriceOracle.sol
+++ Oracle/EthPriceOracle.sol
@@ -67,6 +67,13 @@ contract EthPriceOracle {
 
       uint numResponses = requestIdToResponse[_id].length;
       if (numResponses == THRESHOLD) {
+        uint computedEthPrice = 0;
+        for (uint f=0; f < requestIdToResponse[_id].length; f++) {
+          computedEthPrice += requestIdToResponse[_id][f].ethPrice;
+        } //end for()
+
+        computedEthPrice = computedEthPrice / numResponses;
+        
         delete pendingRequests[_id];
         CallerContractInterface callerContractInstance;
         callerContractInstance = CallerContractInterface(_callerAddress);
