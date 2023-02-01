060dafe59e3af6909006edbd091c92bdc2977d73
Jordan <jordanonuma@outlook.com>
2023-02-01 12:05:08 -1000

    Instantiated zksync.

---
+++ Oracle/Src/Utils.js
@@ -0,0 +1,27 @@
+async function getZkSyncProvider (zksync, networkName) {
+    let zkSyncProvider
+    try {
+      zkSyncProvider = await zksync.getDefaultProvider(networkName)
+    } catch (error) {
+      console.log('Unable to connect to zkSync.')
+      console.log(error)
+    }
+    return zkSyncProvider
+} //end function getZkSyncProvider()
+  
+async function getEthereumProvider (ethers, networkName) {
+    let ethersProvider
+    try {
+        // eslint-disable-next-line new-cap
+        ethersProvider = new ethers.getDefaultProvider(networkName)
+    } catch (error) {
+        console.log('Could not connect to Rinkeby')
+        console.log(error)
+    }
+    return ethersProvider 
+} //end function getEthereumProvider()
+  
+async function initAccount(rinkebyWallet, zkSyncProvider, zksync) {
+    const zkSyncWallet = await zksync.Wallet.fromEthSigner(rinkebyWallet, zkSyncProver)
+    return zkSyncWallet
+} //end function initAccount()
