9f553c7e18c33cc5ed255e7f799c4cf97753fb8a
Jordan <jordanonuma@outlook.com>
2023-02-03 12:18:13 -1000

    Calls function that transfers to zksync. awaitReceipt() is called within try-catch{}.

--- Oracle/Src/Utils.js
+++ Oracle/Src/Utils.js
@@ -38,3 +38,17 @@ async function registerAccount (wallet) {
         await changePubkey.awaitReceipt()
     } //end if()
 } //end function registerAccount()
+
+async function depositToZkSync(zkSyncWallet, token, amountToDeposit, ethers) {
+    const deposit = await zkSyncWallet.depositToSyncFromEthereum({
+        depositTo: zkSyncWallet.address(),
+        token: token,
+        amount: ethers.utils.parseEther(amountToDeposit)
+    }) //depositToSyncFromEthereum()
+    try {
+        await deposit.awaitReceipt()
+    } catch (error) {
+        console.log('Error while awaiting confirmation from the zkSync operators.')
+        console.log(error)
+    } //end try-catch{}
+} //end function depositToZkSync()
