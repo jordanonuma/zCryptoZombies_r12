fd94ab1ecec13f2863c704922c27133a477e3346
Jordan <jordanonuma@outlook.com>
2023-02-08 12:09:18 -1000

    Imports modules necessary to define network provider.

--- Oracle/Src/Bob.js
+++ Oracle/Src/Bob.js
@@ -1,11 +1,8 @@
-module.exports = {
-    getZkSyncProvider,
-    getEthereumProvider,
-    depositToZkSync,
-    registerAccount,
-    displayZkSyncBalance,
-    transfer,
-    withdrawToEthereum,
-    getFee,
-    initAccount
-}
+(async () => {
+    const ethers = require('ethers')
+    const zksync = require('zksync')
+    const utils = require('./utils')
+  
+    const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+})()
