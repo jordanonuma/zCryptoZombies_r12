f8c50b70b6bb5770829f3a6cfbd4523a5090038f
jordan <jordanonuma@outlook.com>
2023-02-09 16:14:59 -1000

    Added balance display.

--- Oracle/Src/Bob.js
+++ Oracle/Src/Bob.js
@@ -8,7 +8,7 @@
   
     const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_KEY, ethersProvider)
     console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`)
+    console.log(`Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await bobRinkebyWallet.getBalance())}`)
     const bobZkSyncWallet = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync)
     
-})
-  
+})()
