2649b7668738e520f32be53aaa1a7b2dca16cf29
jordan <jordanonuma@outlook.com>
2023-02-09 16:11:01 -1000

    Initialized test wallet for Bob on zksync.

--- Oracle/Src/Bob.js
+++ Oracle/Src/Bob.js
@@ -1,11 +1,14 @@
-module.exports = {
-    getZkSyncProvider,
-    getEthereumProvider,
-    depositToZkSync,
-    registerAccount,
-    displayZkSyncBalance,
-    transfer,
-    withdrawToEthereum,
-    getFee,
-    initAccount
-}
+(async () => {
+    const ethers = require('ethers')
+    const zksync = require('zksync')
+    const utils = require('./utils')
+  
+    const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+  
+    const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_KEY, ethersProvider)
+    console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`)
+    const bobZkSyncWallet = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync)
+    
+})
+  
